2fac962cf024c22347cc285716258d43b71ae826
REGUIG Ghiles <ghiles.reguig@icm-cenir-le47.icm-institute.org>
2018-05-23 15:35:02 +0200

    SLURMGraph

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="connectivityworkflow",
-      version="0.0.1",
+      version="0.0.2",
       description="A workflow for graph theory measure calculations from FMRI data",
       url="https://github.com/GReguig/connectivityworkflow",
       author="Reguig Ghiles",
