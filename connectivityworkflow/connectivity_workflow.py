1a21ba3dfa2b2f4afc81196c4a8618989c031d75
REGUIG Ghiles <ghiles.reguig@icm-cenir-le47.icm-institute.org>
2018-05-23 11:02:11 +0200

    Generic version

--- connectivityworkflow/connectivity_workflow.py
+++ connectivityworkflow/connectivity_workflow.py
@@ -6,12 +6,13 @@ Created on Mon May  7 15:56:02 2018
 @author: ghiles.reguig
 """
 import argparse
-from .data_bids_grabber import GetBidsDataGrabberNode
-from .confounds_selector import getConfoundsReaderNode
-from .signal_extraction_freesurfer import SignalExtractionFreeSurfer
-from .connectivity_calculation import ConnectivityCalculation
-from nipype import Workflow, Node
+from data_bids_grabber import GetBidsDataGrabberNode
+from confounds_selector import getConfoundsReaderNode
+from signal_extraction_freesurfer import SignalExtractionFreeSurfer
+from connectivity_calculation import ConnectivityCalculation
+from nipype import Workflow, Node, config
 from os.path import join as opj
+import json
 
 def BuildConnectivityWorkflow(path, outDir):
     #Workflow Initialization
@@ -46,11 +47,17 @@ def BuildConnectivityWorkflow(path, outDir):
             ])
     return connectivityWorkflow
 
-def RunConnectivityWorkflow(path, outDir,workdir):
+def RunConnectivityWorkflow(path, outDir,workdir, conf_file=None):
+    if conf_file is not None : 
+        res=open(conf_file,'r').read()
+        conf=json.loads(res)
+        config.update_config(conf)
+        
+    plugin = config.get("execution","plugin")
+    plugin_args = eval(config.get("execution","plugin_args"))
     wf = BuildConnectivityWorkflow(path,outDir)
     wf.base_dir = workdir
-    wf.run(plugin='SLURMGraph', plugin_args = {'dont_resubmit_completed_jobs': True,
-"sbatch_args":" -e /export/dataCENIR/users/ghiles.reguig/ConnOutput/%j.err -o /export/dataCENIR/users/ghiles.reguig/ConnOutput/%j.out --mem 12G"})
+    wf.run(plugin=plugin, plugin_args=plugin_args)
     
     
     
@@ -61,14 +68,16 @@ if __name__ == "__main__" :
     #Add the filepath argument to the BIDS dataset
     parser.add_argument("-p","--path", dest="path",help="Path to BIDS Dataset", required=True)
     parser.add_argument("-w", "--workdir", dest="workdir", help="Path to working directory", required=False)
+    parser.add_argument("-c", "--config", dest="conf", help="Nipype JSON configuration file ", required=False)
     #Parse the commandline
     args = parser.parse_args()
     #Get the filepath argument specified by the user
     path = args.path
     workdir = args.workdir
+    conf = args.conf
     outDir = opj(path,"derivatives","connectivityWorkflow")
     
     print("Results will be written in {}".format(outDir))
     #Build the workflow
-    RunConnectivityWorkflow(path, outDir, workdir)
+    RunConnectivityWorkflow(path, outDir, workdir, conf)
     
