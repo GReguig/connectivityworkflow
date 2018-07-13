10a742836caca3e5249af9499621e5531f32b42d
REGUIG Ghiles <ghiles.reguig@icm-cenir-le47.icm-institute.org>
2018-07-13 08:50:09 +0200

    Time series filtering surface

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="connectivityworkflow",
-      version="0.0.72",
+      version="0.0.74",
       description="A workflow for graph theory measure calculations from FMRI data",
       url="https://github.com/GReguig/connectivityworkflow",
       author="Reguig Ghiles",
